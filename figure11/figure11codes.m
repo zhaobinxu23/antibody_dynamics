21a3ec819e89d0c4c5fe273032d18cebf241285c
zhaobinxu23 <77838473+zhaobinxu23@users.noreply.github.com>
2021-12-02 14:06:59 +0800

    Add files via upload

---
+++ figure11/figure11codes.m
@@ -0,0 +1,25 @@
+% parametersetting;
+clc
+clear
+x0(1) = 0;% virus_antibody_complex
+x0(2) = 1000;% antibody_level_initial
+x0(3) = 1;% virus_intial_number
+x0(4) = 0; % environment antigen
+x0(5) = 0;% environ_antibody_complex
+
+
+para(1) = 3e-5; 
+para(2) = 1e-14; 
+para(3) = 1000; 
+para(4) = 0.98; 
+para(5) = 1.08;
+para(6) = 1e-9;% environment antigen binding kinetics
+[t, y]=ode15s(@pathway_model_environ,[0 2000],x0,[],para);
+% plot(t,y(:,1),'linewidth',2);
+% hold on
+plot(t,y(:,2),'linewidth',2);
+hold on
+plot(t,y(:,3),'linewidth',2);
+
+
+
